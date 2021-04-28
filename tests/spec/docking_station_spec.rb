9905f17ef1a8903dce9bcfaf17c221bd8e24d7d4
Felix <felixcpiercy@gmail.com>
2021-04-28 11:09:44 +0100

    Created initial Rspec test

---
+++ tests/spec/docking_station_spec.rb
@@ -0,0 +1,7 @@
+describe DockingStation do 
+end 
+
+
+#this still throws up an unitialized constant error because
+#we have not described the DockingStation class, therefore
+#there is still nothing to test
