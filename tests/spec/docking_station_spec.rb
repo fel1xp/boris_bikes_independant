ac733232616e22494061c98c08ea4362a0fef99d
Felix <felixcpiercy@gmail.com>
2021-04-28 15:18:29 +0100

    Step 8, ran first successful test

--- tests/spec/docking_station_spec.rb
+++ tests/spec/docking_station_spec.rb
@@ -1,7 +1,8 @@
-describe DockingStation do 
-end 
+require 'docking_station'
 
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
+end
 
-#this still throws up an unitialized constant error because
-#we have not described the DockingStation class, therefore
-#there is still nothing to test
+# Add a test to your spec file that expects DockingStation instances
+# to respond_to the method release_bike
